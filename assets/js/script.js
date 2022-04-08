833698c57dc9fc02b1323a79732419c8856679d6
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-08 15:02:34 -0300

    feat: inclui função que faz a requisição da api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-document.querySelector('.busca').addEventListener('submit', event => {
+document.querySelector('.busca').addEventListener('submit', async event => {
   event.preventDefault();
 
   let input = document.querySelector('#searchInput').value;
 
   if (input !== '') {
     showWarning('Carregando...');
-  }
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${encondeURI(
-    input
-  )}&appid=e95dc6cf975b104382086af23b711e98&units=metric&lang=pt_br`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+      input
+    )}&appid=e95dc6cf975b104382086af23b711e98&units=metric&lang=pt_br`;
+
+    let results = await fetch(url);
+    let json = await results.json();
+    console.log(json);
+  }
 });
 
 function showWarning(message) {
