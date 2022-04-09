cfecc7c0c50d9c4669712f21354f6e6b5d37da77
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-08 21:28:39 -0300

    feat: inclui função que lista os resultados da requisição na tela

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,23 @@ function showInfo(json) {
   showWarning('');
 
   document.querySelector('.resultado').style.display = 'block';
+
+  document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+  document.querySelector('.tempInfo').innerHTML = `${json.temp}<sup>ºC</sup> `;
+  document.querySelector(
+    '.ventoInfo'
+  ).innerHTML = `${json.windSpeed} <span>km/h</span> `;
+
+  document
+    .querySelector('.temp img')
+    .setAttribute(
+      'src',
+      `https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`
+    );
+
+  document.querySelector('.ventoPonto').style.transform = `rotate(${
+    json.windAngle - 90
+  }deg)`;
 }
 
 function showWarning(message) {
